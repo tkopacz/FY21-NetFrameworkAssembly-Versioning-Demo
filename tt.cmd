31a2699d41735d0165a3b61f1bd0c474e4e977c9
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2021-08-24 10:21:25 +0200

    Work

--- tt.cmd
+++ tt.cmd
@@ -1,3 +1,9 @@
+git stage .
+git commit -m "Work"
+rem git commit -m "Work **BYPASS_SECRET_SCANNING**"
+git push
+
+
 For /f "tokens=1,2,3,4,5 delims=-./ " %%a in ('date /T') do set CDate=%%d-%%c-%%b
 For /f "tokens=1,2,3,4,5 delims=:,. " %%a in ( "%time%" ) do set CTime=%%a-%%b-%%c
 echo %CDate%
